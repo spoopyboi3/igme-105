d910a027565a787b40ebd489453d5bbae8ba80f1
Alex Gayne <atg5699@g.rit.edu>
2022-11-28 09:37:56 -0500

    PE19 started

---
+++ PE19/PE19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+/*Creator: Alex Gayne
+ *Course: IGME 105
+ *Instructor: Ann Warren
+ *Date: 11/28/22
+ *Purpose: Finish PE #19
+ *Modifications:
+*/
+namespace PE19 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+        }
+    }
+}
